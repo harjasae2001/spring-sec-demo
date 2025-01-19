c9309360c21515d01a660e565c3839574fa7d470
Harjas Singh <harjas.singh.dev@gmail.com>
2025-01-19 14:14:12 +0530

    Added files

---
+++ src/main/java/com/example/spring_sec_demo/StudentController.java
@@ -0,0 +1,34 @@
+package com.example.spring_sec_demo;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+public class StudentController {
+
+    List<Student> students = new ArrayList<>(List.of(
+        new Student(1, "navin", "Java"),
+        new Student(2, "kiran", "Blockchain")
+    ));
+
+//    @GetMapping("csrf-token")
+//    public CsrfToken getCsrfToken(HttpServletRequest request){
+//        return (CsrfToken) request.getAttribute("_csrf");
+//    }
+
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @PostMapping("students")
+    public void addstudent(@RequestBody Student student){
+        students.add(student);
+    }
+}
